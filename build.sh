752b94b8dc8ba39d28ce7bb0edb37883bf6dbc1a
magroski <magroski@gmail.com>
2019-05-09 20:27:57 -0300

    Image and build script

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+HUB_USERNAME=magroski
+IMAGE_NAME="${HUB_USERNAME}/php-dev"
+IMAGES_DIR='./docker/images'
+
+scream() {
+    echo
+    echo
+    echo "$@"
+    echo
+    echo
+}
+
+get_last_repo_tag() {
+    # Get last annotated tag
+    # https://stackoverflow.com/questions/1404796/how-to-get-the-latest-tag-name-in-current-branch-in-git
+    # redirect stderr in order to avoid warnings
+    echo $(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
+}
+
+get_tag() {
+    [[ -n "$DOCKER_TAG" ]] && echo $DOCKER_TAG || echo $(get_last_repo_tag)
+}
+
+build_app() {
+    docker build --tag ${IMAGE_NAME}:cli "${IMAGES_DIR}/php-cli"
+    docker tag ${IMAGE_NAME}:cli ${IMAGE_NAME}:cli-$(get_tag)
+}
+
+push_app() {
+    docker push ${IMAGE_NAME}:cli
+    docker push ${IMAGE_NAME}:cli-$(get_tag)
+}
+
+scream "Building Flux's images with tag $(get_tag)"
+build_app
